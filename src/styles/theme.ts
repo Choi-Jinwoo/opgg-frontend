c4086be20500427afd24d94d59c7069799a0092e
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-21 18:04:53 +0900

    add: style theme 정의

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+const theme = {
+  colors: {
+    amethyst: '#8c51c5',
+    azure: '#1ea1f7',
+    blue: '#1f8ecd',
+    red: '#c6443e',
+    orange: '#e19205',
+    green: '#2daf7f',
+    white: '#ffffff',
+    black: '#242929',
+
+    gray1: '#f2f2f2',
+    gray2: '#eaeaea',
+    gray3: '#cdd2d2',
+    gray4: '#b6b6b6',
+    gray5: '#a7a7a7',
+    gray6: '#879292',
+    gray7: '#5e5e5e',
+    gray8: '#555555',
+  },
+  fontSizes: {
+    large: '20px',
+    regular: '16px',
+    medium: '15px',
+    small: '12px',
+    tiny: '11px',
+  },
+  innerSizes: {
+    desktop: '1000px',
+  },
+};
