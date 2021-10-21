3ae2de4957fd0edfe149ab8f05be6f4f552f6b90
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-21 19:52:30 +0900

    add: Text 공동 컴포넌트

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import theme from '@/styles/theme';
+import styled from 'styled-components';
+import { FontWeight } from '@/styles/types';
+
+type Props = {
+  color?: string;
+  fontSize?: string;
+  fontWeight?: FontWeight;
+};
+
+const Text: React.FC<Props> = ({
+  children,
+  color = theme.colors.black,
+  fontSize = theme.fontSizes.regular,
+  fontWeight = 'bold',
+}) => {
+  return (
+    <Span color={color} fontSize={fontSize} fontWeight={fontWeight}>
+      {children}
+    </Span>
+  );
+};
+
+const Span = styled.span<{
+  color: string;
+  fontSize: string;
+  fontWeight: string;
+}>`
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+`;
+
+export default Text;
