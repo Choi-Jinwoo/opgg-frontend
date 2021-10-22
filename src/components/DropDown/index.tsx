7744d5ae43f2c9674795ba88a8599509a1e91201
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-22 10:57:07 +0900

    add: DropDown 컴포넌트 isVisible을 통한 조건부 렌더링

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 type Props = {
   // pixel
   top: number;
+  isVisible: boolean;
 };
 
 // Parent should be relative position
-const DropDown: React.FC<Props> = ({ children, top }) => {
-  return <Container top={top}>{children}</Container>;
+const DropDown: React.FC<Props> = ({ children, top, isVisible }) => {
+  return (
+    <Container isVisible={isVisible} top={top}>
+      {children}
+    </Container>
+  );
 };
 
 type ContainerProps = {
   top: number;
+  isVisible: boolean;
 };
 
 const Container = styled.div<ContainerProps>`
+  ${(props) =>
+    !props.isVisible &&
+    css`
+      display: none;
+    `}
+
   position: absolute;
   width: 100%;
   top: ${(props) => props.top}px;
