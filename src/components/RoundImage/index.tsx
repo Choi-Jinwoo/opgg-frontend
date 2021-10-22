fb231314bc4c3306dfb48cf5de3f65ba46ff8802
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-22 09:59:29 +0900

    add: RoundImage 공통 컴포넌트

---
+++ src/components/RoundImage/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  // pixel
+  width?: number;
+  src: string;
+  alt: string;
+};
+
+const RoundImage: React.FC<Props> = ({ src, alt, width = 46 }) => {
+  return <Image src={src} alt={alt} width={width} />;
+};
+
+const Image = styled.img`
+  width: ${(props) => props.width};
+  border-radius: 50%;
+`;
+
+export default RoundImage;
