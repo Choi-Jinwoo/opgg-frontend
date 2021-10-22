fe7af883050812307a52c803f710a5eae9fd285f
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-22 19:56:15 +0900

    add: Summoner Fetch Store, Repository 클래스 작성

---
+++ src/repository/summoner.ts
@@ -0,0 +1,19 @@
+import { SummonerAttributes } from '@src/models/summoner';
+import { AxiosResponse } from 'axios';
+import axiosInstance from './axios';
+
+type FetchSummonerResponse = {
+  summoner: SummonerAttributes;
+};
+
+class SummonerRepository {
+  fetchSummoner(
+    summonerName: string,
+  ): Promise<AxiosResponse<FetchSummonerResponse>> {
+    return axiosInstance.get<FetchSummonerResponse>(
+      `/summoner/${summonerName}`,
+    );
+  }
+}
+
+export default new SummonerRepository();
---
+++ src/stores/summonerStore.ts
@@ -0,0 +1,23 @@
+import Summoner from '@src/models/summoner';
+import { action, makeAutoObservable, observable, runInAction } from 'mobx';
+import summonerRepository from '@src/repository/summoner';
+
+class SummonerStore {
+  @observable
+  summoner: Summoner | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  @action
+  async fetchSummoner(summonerName: string) {
+    const { data } = await summonerRepository.fetchSummoner(summonerName);
+
+    runInAction(() => {
+      this.summoner = Summoner.from(data.summoner);
+    });
+  }
+}
+
+export default new SummonerStore();
