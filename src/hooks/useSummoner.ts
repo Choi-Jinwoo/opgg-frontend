8488f56ca819752b3105b402eeb6bbbd4606c9e3
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-22 20:09:20 +0900

    add: 소환사 정보 조회 useSummoner hooks

---
+++ src/hooks/useSummoner.ts
@@ -0,0 +1,19 @@
+import Summoner from '@src/models/summoner';
+import summonerStore from '@src/stores/summonerStore';
+import { useEffect } from 'react';
+
+const useSummoner = (): Summoner | null => {
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const summonerName = query.get('summoner');
+
+    // querystring 없을 경우 처리
+    if (summonerName === null) return;
+
+    summonerStore.fetchSummoner(summonerName).catch(); // 오류 처리
+  }, []);
+
+  return summonerStore.summoner;
+};
+
+export default useSummoner;
