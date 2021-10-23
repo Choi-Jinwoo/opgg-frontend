7e1bf5537b7e8722133ba8ca570251ca619b8cf1
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 12:38:43 +0900

    fix: Summoer mapping 수정

--- src/models/summoner.ts
+++ src/models/summoner.ts
@@ -16,14 +16,15 @@ export class SummonerAttributes {
     hasResults: boolean;
     losses: number;
     wins: number;
-    tierRank: TierRank[];
-  };
+    tierRank: TierRank;
+  }[];
 
   constructor(summoner: SummonerAttributes) {
     this.name = summoner.name;
     this.level = summoner.level;
     this.profileBackgroundImageUrl = summoner.profileBackgroundImageUrl;
     this.profileBorderImageUrl = summoner.profileBorderImageUrl;
+    this.previousTiers = summoner.previousTiers;
     this.profileImageUrl = summoner.profileImageUrl;
     this.url = summoner.url;
     this.ladderRank = summoner.ladderRank;
@@ -33,9 +34,13 @@ export class SummonerAttributes {
 
 class Summoner extends SummonerAttributes {
   static from(summoner: SummonerAttributes): Summoner {
-    summoner.previousTiers = summoner.previousTiers.map(TierRank.from);
-    summoner.leagues.tierRank = summoner.leagues.tierRank.map(TierRank.from);
-
+    summoner.previousTiers = summoner.previousTiers.map((tier) =>
+      TierRank.from(tier),
+    );
+    summoner.leagues = summoner.leagues.map((league) => ({
+      ...league,
+      tierRank: TierRank.from(league.tierRank),
+    }));
     return new Summoner(summoner);
   }
 }
