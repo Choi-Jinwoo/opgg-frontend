ffbbc51d3aa7d46c970c526e40df5e9ed6bb51b6
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 14:29:27 +0900

    add: Champion 모델 정의

---
+++ src/models/champion.ts
@@ -0,0 +1,41 @@
+export class ChampionAttributes {
+  id!: number;
+  key!: string;
+  name!: string;
+  imageUrl!: string;
+  games!: number;
+  kills!: number;
+  deaths!: number;
+  assists!: number;
+  wins!: number;
+  losses!: number;
+
+  constructor(champion: ChampionAttributes) {
+    this.id = champion.id;
+    this.key = champion.key;
+    this.name = champion.name;
+    this.imageUrl = champion.imageUrl;
+    this.games = champion.games;
+    this.kills = champion.kills;
+    this.deaths = champion.deaths;
+    this.assists = champion.assists;
+    this.wins = champion.wins;
+    this.losses = champion.losses;
+  }
+}
+
+class Champion extends ChampionAttributes {
+  static from(champion: ChampionAttributes): Champion {
+    return new Champion(champion);
+  }
+
+  get winningRate(): string {
+    return ((this.wins / this.games) * 100).toFixed(0);
+  }
+
+  getKDA(): string {
+    return (this.kills + this.assists / this.deaths).toFixed(2);
+  }
+}
+
+export default Champion;
