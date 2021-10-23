49f0dd7336298140b6977d0bb6b56f7880a91976
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 20:02:52 +0900

    add: SummaryChart에 추가 정보 UI 표시

--- src/components/MatchSummary/SummaryChart/index.tsx
+++ src/components/MatchSummary/SummaryChart/index.tsx
@@ -1,34 +1,93 @@
 import React from 'react';
 import PieChart from '@src/components/PieChart';
 import theme from '@src/styles/theme';
+import styled from 'styled-components';
+import Text from '@src/components/Text';
+import matchStore from '@src/stores/matchStore';
+import { observer } from 'mobx-react';
+import Emphasize from '@src/components/Emphasize';
+import ConditionalColorFont from '@src/components/ConditionalColorFont';
+import kdaColorCondition from '@src/lib/conditionalColor/KDAColorCondition';
 
-type Props = {
-  wins: number;
-  losses: number;
-  winningRate: number;
-};
+const SummaryChart: React.FC = () => {
+  if (matchStore.summonerMatchSummary === null) {
+    // TODO: 로딩 적용
+    return <div>없음</div>;
+  }
 
-const SummaryChart: React.FC<Props> = ({ wins, losses, winningRate }) => {
+  const {
+    wins,
+    losses,
+    winningRate,
+    totalMatches,
+    kills,
+    deaths,
+    assists,
+    kda,
+  } = matchStore.summonerMatchSummary;
+
   return (
-    <PieChart
-      size={100}
-      title={`${winningRate}%`}
-      bgColor={theme.colors.gray2}
-      titleColor={theme.colors.gray7}
-      data={[
-        {
-          key: 'wins',
-          value: wins,
-          color: theme.colors.blue,
-        },
-        {
-          key: 'loses',
-          value: losses,
-          color: theme.colors.lightRed,
-        },
-      ]}
-    />
+    <Container>
+      <ChartWrapper>
+        <Text
+          fontSize={theme.fontSizes.medium}
+          color={theme.colors.gray7}
+          marginBottom={8}
+        >{`${totalMatches}전 ${wins}승 ${losses}패`}</Text>
+        <PieChart
+          size={100}
+          title={`${winningRate}%`}
+          bgColor={theme.colors.gray2}
+          titleColor={theme.colors.gray7}
+          data={[
+            {
+              key: 'wins',
+              value: wins,
+              color: theme.colors.blue,
+            },
+            {
+              key: 'loses',
+              value: losses,
+              color: theme.colors.lightRed,
+            },
+          ]}
+        />
+      </ChartWrapper>
+
+      <KDAWrapper>
+        <Text
+          fontSize={theme.fontSizes.tiny}
+          fontWeight="bold"
+          marginBottom={8}
+        >
+          {kills}/<Emphasize color={theme.colors.red}>{deaths}</Emphasize>/
+          {assists}
+        </Text>
+        <ConditionalColorFont value={kda} condition={kdaColorCondition}>
+          <Text fontSize={theme.fontSizes.regular}>{`${kda}:1`}</Text>
+        </ConditionalColorFont>
+      </KDAWrapper>
+    </Container>
   );
 };
 
-export default SummaryChart;
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChartWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  align-items: center;
+`;
+
+const KDAWrapper = styled.div`
+  margin-left: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default observer(SummaryChart);
