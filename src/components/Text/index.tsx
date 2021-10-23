06744b219d58c02b12edf63d06e9c55fcc2a9c98
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 12:39:57 +0900

    add: Text 컴포넌트 margin 값 추가

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -7,6 +7,11 @@ type Props = {
   color?: string;
   fontSize?: string;
   fontWeight?: FontWeight;
+  // margin: pixel
+  marginLeft?: number;
+  marginRight?: number;
+  marginTop?: number;
+  marginBottom?: number;
 };
 
 const Text: React.FC<Props> = ({
@@ -14,9 +19,21 @@ const Text: React.FC<Props> = ({
   color = theme.colors.black,
   fontSize = theme.fontSizes.regular,
   fontWeight = 'normal',
+  marginLeft = 0,
+  marginRight = 0,
+  marginTop = 0,
+  marginBottom = 0,
 }) => {
   return (
-    <Span color={color} fontSize={fontSize} fontWeight={fontWeight}>
+    <Span
+      color={color}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      marginLeft={marginLeft}
+      marginRight={marginRight}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+    >
       {children}
     </Span>
   );
@@ -26,10 +43,18 @@ const Span = styled.span<{
   color: string;
   fontSize: string;
   fontWeight: string;
+  marginLeft?: number;
+  marginRight?: number;
+  marginTop?: number;
+  marginBottom?: number;
 }>`
   color: ${(props) => props.color};
   font-size: ${(props) => props.fontSize};
   font-weight: ${(props) => props.fontWeight};
+  margin-left: ${(props) => props.marginLeft}px;
+  margin-right: ${(props) => props.marginRight}px;
+  margin-top: ${(props) => props.marginTop}px;
+  margin-bottom: ${(props) => props.marginBottom}px;
 `;
 
 export default Text;
