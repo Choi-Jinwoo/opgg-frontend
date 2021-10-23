dcea52645514febe83eea18455d688f10207cccd
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 15:45:57 +0900

    fix: MatchStore 생성 시 make obserable 실행

--- src/stores/matchStore.ts
+++ src/stores/matchStore.ts
@@ -1,10 +1,14 @@
 import Champion from '@src/models/champion';
-import { action, observable, runInAction } from 'mobx';
+import { action, makeAutoObservable, observable, runInAction } from 'mobx';
 import matchRepository from '@src/repository/match';
 
 class MatchStore {
   @observable
   mostChampions: Champion[] | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 
   @action
   async fetchMatches(summonerName: string) {
