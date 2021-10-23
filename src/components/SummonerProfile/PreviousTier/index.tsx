ebc5d78ef671c8eb999ba916b8f7880bbf9ccfcf
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 12:41:59 +0900

    add: 이전 시즌 티어 PreviousTiers 컴포넌트

---
+++ src/components/SummonerProfile/PreviousTier/index.tsx
@@ -0,0 +1,26 @@
+import TierRank from '@src/models/tierRank';
+import React from 'react';
+import styled from 'styled-components';
+import TierBadge from '../TierBadge';
+
+type Props = {
+  tiers: TierRank[];
+};
+
+const PreviousTiers: React.FC<Props> = ({ tiers }) => {
+  const tierBadges = tiers.map((tier, index) => (
+    <TierBadge key={index} tier={tier} />
+  ));
+
+  return <Container>{tierBadges}</Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+
+  & > * + * {
+    margin-left: 8px;
+  }
+`;
+
+export default PreviousTiers;
