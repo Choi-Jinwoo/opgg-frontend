a7ebbf05654aa4dc285d4ef0d9436b96d636e3b3
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-23 18:01:53 +0900

    add: MostUsedChampions 컴포넌트

---
+++ src/components/MatchSummary/MostUsedChampions/MostUsedChampionItem/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import RoundImage from '@src/components/RoundImage';
+import Champion from '@src/models/champion';
+import Text from '@src/components/Text';
+import theme from '@src/styles/theme';
+import ConditionalColorFont from '@src/components/ConditionalColorFont';
+import winningRateColorCondition from '@src/lib/conditionalColor/WinningRateColorCondition';
+import kdaColorCondition from '@src/lib/conditionalColor/KDAColorCondition';
+
+type Props = {
+  champion: Champion;
+};
+
+const MostUsedChampionItem: React.FC<Props> = ({ champion }) => {
+  const { imageUrl, name, winningRate, wins, losses, kda } = champion;
+
+  return (
+    <Container>
+      <RoundImage width={34} src={imageUrl} alt={name} />
+
+      <ChampionInfoWrapper>
+        <Text fontSize={theme.fontSizes.medium}>{name}</Text>
+
+        <ChampionRateWrapper>
+          <ConditionalColorFont
+            value={winningRate}
+            condition={winningRateColorCondition}
+          >
+            <Text fontWeight="bold" fontSize={theme.fontSizes.tiny}>
+              {winningRate}%
+            </Text>
+          </ConditionalColorFont>
+
+          <Text
+            fontSize={theme.fontSizes.tiny}
+            color={theme.colors.gray8}
+            marginLeft={8}
+          >{`(${wins}승 ${losses}패)`}</Text>
+
+          <ConditionalColorFont
+            value={Number(kda)}
+            condition={kdaColorCondition}
+          >
+            <Text fontSize={theme.fontSizes.tiny} marginLeft={8}>
+              {kda} 평점
+            </Text>
+          </ConditionalColorFont>
+        </ChampionRateWrapper>
+      </ChampionInfoWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.li`
+  margin: 8px 0px;
+  display: flex;
+  align-items: center;
+`;
+
+const ChampionInfoWrapper = styled.div`
+  margin-left: 8px;
+`;
+
+const ChampionRateWrapper = styled.div``;
+
+export default MostUsedChampionItem;
---
+++ src/components/MatchSummary/MostUsedChampions/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import matchStore from '@src/stores/matchStore';
+import MostUsedChampionItem from './MostUsedChampionItem';
+import styled from 'styled-components';
+import NoChampionData from './NoChampionData';
+
+const MAX_MOST_USED_CHAMPIONS = 3;
+
+const MostUsedChampions: React.FC = () => {
+  const { mostChampions } = matchStore;
+
+  if (mostChampions === null) {
+    return <div>없음</div>;
+  }
+
+  const mostUsedChampionItems = mostChampions.map((champion) => (
+    <MostUsedChampionItem key={champion.key} champion={champion} />
+  ));
+
+  const NoChampionDataItems = new Array(
+    MAX_MOST_USED_CHAMPIONS - mostChampions.length,
+  )
+    .fill(null)
+    .map((_, index) => <NoChampionData key={index} />);
+
+  return (
+    <Container>
+      {mostUsedChampionItems}
+      {NoChampionDataItems}
+    </Container>
+  );
+};
+
+const Container = styled.ul`
+  padding: 20px;
+`;
+
+export default observer(MostUsedChampions);
