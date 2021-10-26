6a25073be303974f44700ed889c789d8ea0636ed
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-26 20:01:08 +0900

    add: MostChampion 모델

---
+++ src/models/mostChampion.ts
@@ -0,0 +1,47 @@
+export class MostChampionAttributes {
+  id: number;
+  key: string;
+  name: string;
+  imageUrl: string;
+  games: number;
+  kills: number;
+  deaths: number;
+  assists: number;
+  wins: number;
+  losses: number;
+  cs: number;
+  rank: number;
+
+  constructor(mostChampion: MostChampionAttributes) {
+    this.id = mostChampion.id;
+    this.key = mostChampion.key;
+    this.name = mostChampion.name;
+    this.imageUrl = mostChampion.imageUrl;
+    this.games = mostChampion.games;
+    this.kills = mostChampion.kills;
+    this.deaths = mostChampion.deaths;
+    this.assists = mostChampion.assists;
+    this.wins = mostChampion.wins;
+    this.losses = mostChampion.losses;
+    this.cs = mostChampion.cs;
+    this.rank = mostChampion.rank;
+  }
+}
+
+class MostChampion extends MostChampionAttributes {
+  static from(mostChampion: MostChampionAttributes): MostChampion {
+    return new MostChampion(mostChampion);
+  }
+
+  get kda(): string {
+    if (this.deaths === 0) return 'Perfect';
+
+    return ((this.kills + this.assists) / this.deaths).toFixed(2);
+  }
+
+  get winningRate(): string {
+    return (this.wins / this.games).toFixed(0);
+  }
+}
+
+export default MostChampion;
