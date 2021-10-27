d0c141f78a7b29935497d9e8ff517ea85b8f0548
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-27 09:48:03 +0900

    add: RecentChampionItem 리스트 RecentChampions 컴포넌트

---
+++ src/components/MostInfo/RecentChampions/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import mostInfoStore from '@src/stores/mostInfoStore';
+import RecentChampionItem from './RecentChampionItem';
+import styled from 'styled-components';
+import { observer } from 'mobx-react';
+
+const RecentChampions: React.FC = () => {
+  const { recentChampions } = mostInfoStore;
+
+  if (recentChampions === null) {
+    return <div>없음</div>;
+  }
+
+  const recentChampionItems = recentChampions.map((recentChampion, index) => (
+    <RecentChampionItem key={index} champion={recentChampion} />
+  ));
+
+  return <Container>{recentChampionItems}</Container>;
+};
+
+const Container = styled.ul`
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.gray2};
+`;
+
+export default observer(RecentChampions);
