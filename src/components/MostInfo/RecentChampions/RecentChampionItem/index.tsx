c52cd4d50c707816362030aead3ac690b40b25d6
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-27 09:47:01 +0900

    add: RecentChampionItem의 UI 목업을 데이터 Props로 받아 UI에 표시

--- src/components/MostInfo/RecentChampions/RecentChampionItem/index.tsx
+++ src/components/MostInfo/RecentChampions/RecentChampionItem/index.tsx
@@ -1,33 +1,36 @@
 import PercentBar from '@src/components/common/PercentBar';
 import RoundImage from '@src/components/common/RoundImage';
 import Text from '@src/components/common/Text';
+import RecentChampion from '@src/models/recentChampion';
 import theme from '@src/styles/theme';
 import React from 'react';
 import styled from 'styled-components';
 
-const RecentChampionItem: React.FC = () => {
+type Props = {
+  champion: RecentChampion;
+};
+
+const RecentChampionItem: React.FC<Props> = ({ champion }) => {
+  const { imageUrl, winningRate, wins, losses, name } = champion;
+
   return (
     <Container>
-      <RoundImage
-        width={32}
-        src="https://opgg-static.akamaized.net/images/lol/champion/Veigar.png?image=q_auto&v=1591083841"
-        alt=""
-      />
-      <ChampionName>신지드</ChampionName>
-      <Text fontSize={theme.fontSizes.tiny} color={theme.colors.gray3}>
-        69%
+      <RoundImage width={32} src={imageUrl} alt={name} />
+      <ChampionName>{name}</ChampionName>
+      <Text fontSize={theme.fontSizes.tiny} color={theme.colors.gray6}>
+        {winningRate}%
       </Text>
       <PercentBar
         width={123}
         dataA={{
-          value: 6,
+          value: wins,
           color: theme.colors.blue,
-          label: '6승',
+          label: `${wins}승`,
         }}
         dataB={{
-          value: 7,
+          value: losses,
           color: theme.colors.lightRed,
-          label: '7패',
+          label: `${losses}패`,
         }}
       />
     </Container>
@@ -51,7 +54,7 @@ const ChampionName = styled.span`
   color: ${(props) => props.theme.colors.gray7};
   font-weight: bold;
   overflow: hidden;
-  width: 40px;
+  width: 52px;
   white-space: nowrap;
   text-overflow: ellipsis;
 `;
